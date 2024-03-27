3e9bbcf3a6a86cd79df6cbb344186aa5c3f18d4c
shingchunglee <shingchung.lee@student.uts.edu.au>
2024-03-27 17:44:33 +1100

    add HUD scripts

---
+++ Assets/Scripts/HUD/ClubSelectManager.cs
@@ -0,0 +1,18 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class ClubSelectManager : MonoBehaviour
+{
+  [SerializeField] TextMeshProUGUI clubNameText;
+
+  private void Awake()
+  {
+    InventoryController.OnClubChanged += OnClubChanged;
+  }
+
+  private void OnClubChanged(Club club)
+  {
+    clubNameText.text = club.clubName;
+  }
+}
