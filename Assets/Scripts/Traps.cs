1545659d900aa662fb0f197b77c2c643faaa95fa
RuviniDharmasena <126440617+RuviniDharmasena@users.noreply.github.com>
2024-04-05 16:14:57 +1100

    traps script

---
+++ Assets/Scripts/Traps.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Traps : MonoBehaviour
+{
+    private PlayerHealth playerHealth;
+
+    private void Start()
+    {
+        playerHealth = FindObjectOfType<PlayerHealth>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Trap"))
+        {
+            playerHealth?.LoseLife(); 
+        }
+    }
+}
