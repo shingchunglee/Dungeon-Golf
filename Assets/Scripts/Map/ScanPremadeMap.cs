91c2200e084d0f8a2e2e968ae58eb506f1e37d62
Mitch Munro <mitchmunro@gmail.com>
2024-04-16 15:05:40 +1000

    Finished testing for ScanPremadeMap

--- Assets/Scripts/Map/ScanPremadeMap.cs
+++ Assets/Scripts/Map/ScanPremadeMap.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -28,8 +29,13 @@ public class ScanPremadeTilemap : MonoBehaviour
     // The origin can go below this if the tile map has tiles below [0,0].
     public Vector2Int gridOrigin = Vector2Int.zero;
 
-
     private void Start()
+    {
+        ScanTilemaps();
+        LogGrid();
+    }
+
+    private void ScanTilemaps()
     {
         if (ReadFromTilemap)
         {
@@ -58,49 +64,116 @@ public class ScanPremadeTilemap : MonoBehaviour
 
             grid = new TileType[gridSize.x, gridSize.y];
 
-            Debug.Log($"Largest size tilemap is [{gridSize}].");
-
-            foreach (var tilemap in tilemaps)
-            {
-                BoundsInt bounds = tilemap.cellBounds;
-                TileBase[] allTiles = tilemap.GetTilesBlock(bounds);
-
-                Debug.Log($"Map: {tilemap.gameObject.name} | Origin: [{bounds.x},{bounds.y}], Size: [{bounds.size.x}, {bounds.size.y}] ==================================================================");
-
-                for (int x = 0; x < bounds.size.x; x++)
-                {
-                    for (int y = 0; y < bounds.size.y; y++)
-                    {
-                        TileBase tile = allTiles[x + y * bounds.size.x];
-
-                        int offsetX = 0;
-                        int offsetY = 0;
-
-                        if (bounds.x != gridOrigin.x)
-                        {
-                            offsetX = Mathf.Abs(gridOrigin.x) - Mathf.Abs(bounds.x);
-                        }
-
-                        if (bounds.y != gridOrigin.y)
-                        {
-                            offsetY = Mathf.Abs(gridOrigin.y) - Mathf.Abs(bounds.y);
-                        }
-
-                        if (tile != null)
-                        {
-                            Debug.Log($"Map: {tilemap.gameObject.name}, Relative: [{x},{y}], Grid: [{x + offsetX},{y + offsetY}] tile: {tile.name}");
-                        }
-                        else
-                        {
-                            Debug.Log($"Map: {tilemap.gameObject.name}, Relative: [{x},{y}], Grid: [{x + offsetX},{y + offsetY}] tile: null");
-
-                        }
-                    }
-                }
-            }
-
+            ScanTilemapByLayer(map_floor, TileType.FLOOR);
+            ScanTilemapByLayer(map_wall, TileType.WALL);
+            ScanTilemapByLayer(map_obstacles, TileType.OBSTACLE);
+            ScanTilemapByLayer(map_trap_damamge, TileType.TRAP_DAMAGE);
+            ScanTilemapByLayer(map_trap_void, TileType.TRAP_VOID);
+            ScanTilemapByLayer(map_trap_floor, TileType.TRAP_FLOOR);
+            ScanTilemapByLayer(map_trap_chest, TileType.TRAP_CHEST);
+            ScanTilemapByLayer(map_chest, TileType.CHEST);
+            ScanTilemapByLayer(map_enemy_spawn, TileType.ENEMY_SPAWN);
 
 
         }
+    }
+
+    private void ScanTilemapByLayer(Tilemap tilemap, TileType tileType)
+    {
+        BoundsInt bounds = tilemap.cellBounds;
+        TileBase[] allTiles = tilemap.GetTilesBlock(bounds);
+
+        // Debug.Log($"Map: {tilemap.gameObject.name} | Origin: [{bounds.x},{bounds.y}], Size: [{bounds.size.x}, {bounds.size.y}] ==================================================================");
+
+        for (int x = 0; x < bounds.size.x; x++)
+        {
+            for (int y = 0; y < bounds.size.y; y++)
+            {
+                TileBase tile = allTiles[x + y * bounds.size.x];
+
+                int offsetX = 0;
+                int offsetY = 0;
+
+                if (bounds.x != gridOrigin.x)
+                {
+                    offsetX = Mathf.Abs(gridOrigin.x) - Mathf.Abs(bounds.x);
+                }
+
+                if (bounds.y != gridOrigin.y)
+                {
+                    offsetY = Mathf.Abs(gridOrigin.y) - Mathf.Abs(bounds.y);
+                }
+
+                if (tile != null)
+                {
+                    // Debug.Log($"Map: {tilemap.gameObject.name}, Relative: [{x},{y}], "
+                    // + $"Grid: [{x + offsetX},{y + offsetY}] tile: {tile.name}");
+
+                    grid[x + offsetX, y + offsetY] = tileType;
+                }
+                else
+                {
+                    // Debug.Log($"Map: {tilemap.gameObject.name}, Relative: [{x},{y}], "
+                    // + "Grid: [{x + offsetX},{y + offsetY}] tile: null");
+                }
+            }
+        }
+    }
+
+    private void LogGrid()
+    {
+        Debug.Log($"Largest size tilemap is [{gridSize}].");
+
+        string mapString = "";
+
+        //GetLength(1) gets the number of elements on the y axis.
+        for (int y = grid.GetLength(1) - 1; y > -1; y--)
+        {
+            string mapLine = "";
+            for (int x = 0; x < grid.GetLength(0); x++)
+            {
+                switch (grid[x, y])
+                {
+                    case TileType.EMPTY:
+                        mapLine += " X";
+                        break;
+                    case TileType.FLOOR:
+                        mapLine += " _ ";
+                        break;
+                    case TileType.WALL:
+                        mapLine += " #";
+                        break;
+                    case TileType.OBSTACLE:
+                        mapLine += " O";
+                        break;
+                    case TileType.TRAP_CHEST:
+                        mapLine += " C";
+                        break;
+                    case TileType.TRAP_DAMAGE:
+                        mapLine += " D";
+                        break;
+                    case TileType.TRAP_FLOOR:
+                        mapLine += " D";
+                        break;
+                    case TileType.TRAP_VOID:
+                        mapLine += " V";
+                        break;
+                    case TileType.ENEMY_SPAWN:
+                        mapLine += " S";
+                        break;
+                    case TileType.CHEST:
+                        mapLine += " C";
+                        break;
+                    default:
+                        mapLine += " ?";
+                        break;
+                }
+            }
+
+            mapString += mapLine + "\n";
+
+        }
+        Debug.Log("Map scan results: \n" +
+            mapString);
     }
 }
