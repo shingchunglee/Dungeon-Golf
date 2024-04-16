12aa58d60d0b40a4d740d275209d389ae7418ede
Kingshuk-MAC <13983919@student.uts.edu.au>
2024-04-16 14:55:34 +1000

    Implementing the code for player respawning after their last shot and losing health if the fall of the map

--- Assets/Scripts/ParSystem.cs
+++ Assets/Scripts/ParSystem.cs
@@ -11,17 +11,22 @@ public class NewBehaviourScript : MonoBehaviour
     private int strokesTaken = 0; 
     public TextMeshProUGUI parText;
     public PlayerHealth playerHealth;
-    public float damagePerStrokeOverPar = 5f; 
+    public float damagePerStrokeOverPar = 5f;
+    public Transform playerTransform;
+    private Vector3 lastShotPosition;
 
     void Start()
     {
         updateParText();
+        lastShotPosition = playerTransform.position; 
     }
 
     public void oneStroke()
     {
         strokesTaken++;
+        lastShotPosition = playerTransform.position; 
         updateParText();
+        playerHealth.SetLastShotPosition(playerTransform.position);
 
         if (strokesTaken > Par)
         {
@@ -33,4 +38,18 @@ public class NewBehaviourScript : MonoBehaviour
     {
         parText.text = strokesTaken.ToString() + "/" + Par.ToString();
     }
+    
+    void OnTriggerExit(Collider other) // Trigger is to check if player leaves an invisible boundary
+    {
+        if (other.CompareTag("Player")) // Make sure the player has a tag "Player"
+        {
+            playerHealth.TakeDamage(20); 
+            if (playerHealth.health <= 0)
+            {
+                playerHealth.Respawn(); 
+            }
+            
+        }
+    }
 }
+
