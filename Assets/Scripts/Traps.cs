15fd42232cb4e2ec57706306a35fa1602cc4513b
shingchunglee <shingchung.lee@student.uts.edu.au>
2024-04-21 11:12:19 +1000

    update traps

--- Assets/Scripts/Traps.cs
+++ Assets/Scripts/Traps.cs
@@ -1,21 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Traps : MonoBehaviour
 {
-    private PlayerHealth playerHealth;
-
-    private void Start()
-    {
-        playerHealth = FindObjectOfType<PlayerHealth>();
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Trap"))
         {
-            playerHealth?.TakeDamage(30); 
+            PlayerManager.Instance.TakeDamage(15);
         }
     }
 }
