965dea8df5276e9ed73d5109031580808f44fc83
shingchunglee <shingchung.lee@student.uts.edu.au>
2024-04-21 15:33:15 +1000

    fix trap damage

--- Assets/Scripts/Traps.cs
+++ Assets/Scripts/Traps.cs
@@ -4,6 +4,7 @@ public class Traps : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        Debug.Log("Player collided with Trap");
         if (collision.gameObject.CompareTag("Trap"))
         {
             PlayerManager.Instance.TakeDamage(15);
