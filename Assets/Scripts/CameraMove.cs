31d1d020896345fe97d161f90a1ae182e73a3e04
Kingshuk-MAC <13983919@student.uts.edu.au>
2024-04-29 16:59:37 +1000

    Updated Free camera to follow the Golfball

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 public class CameraMove : MonoBehaviour
 {
     public float moveSpeed = 5.0f; 
-    public Transform golfBall; 
+    public Transform golfBall;
+    private bool isFreeCamera = false;
 
     void Update()
     {
@@ -11,18 +12,22 @@ public class CameraMove : MonoBehaviour
         
         if (Input.GetKey(KeyCode.W))
         {
+            isFreeCamera = true;
             move.y += 1;
         }
         if (Input.GetKey(KeyCode.S))
         {
+            isFreeCamera = true;
             move.y -= 1;
         }
         if (Input.GetKey(KeyCode.A))
         {
+            isFreeCamera = true;
             move.x -= 1;
         }
         if (Input.GetKey(KeyCode.D))
         {
+            isFreeCamera = true;
             move.x += 1;
         }
 
@@ -30,7 +35,14 @@ public class CameraMove : MonoBehaviour
         
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            isFreeCamera = false;
             ResetCameraPosition();
+        }
+
+        if (!isFreeCamera)
+        {
+            transform.position = new Vector3(PlayerManager.Instance.playerBall.transform.position.x,
+                PlayerManager.Instance.playerBall.transform.position.y, transform.position.z);
         }
     }
 
