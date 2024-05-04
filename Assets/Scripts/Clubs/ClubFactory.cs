6f0598cf02d731fb4f3984c97cf429a1cc6e214b
RuviniDharmasena <126440617+RuviniDharmasena@users.noreply.github.com>
2024-05-04 19:08:41 +1000

    40 clubs update

--- Assets/Scripts/Clubs/ClubFactory.cs
+++ Assets/Scripts/Clubs/ClubFactory.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 // [Serializable]
 public enum ClubType
 {
-  Iron7, Iron8, baronsBalance, excaliberSEdge, ironOfStarfall, merlinsWhisper, mysticsMallet, whackOfWarlock, royalRumble, oraclesOddballPutter, arcaneStrike, wizardsWind,
-  phantomSwing, LegendaryClub, wedgeOfSarcophagus, anubisDive, ironOfNile, boltOfZeus, aetheriusIron, zephyrSwing, ironOfTitan
+  stonePutter, stoneWedge, stoneDriver, Iron7, 
+  ironPutter, ironWedge, ironDriver, excaliberSEdge, 
+  bronzePutter, bronzeWedge, bronzeDriver, baronsBalance,
+  silverPutter, silverWedge, silverDriver, merlinsWhisper,
+  moonironPutter, moonironWedge, moonironDriver, ironOfStarfall,
+  elvensteelPutter, elvensteelWedge, elvensteelDriver,  mysticsMallet,
+  dragonbonePutter, dragonboneWedge, dragonboneDriver, royalRumble,
+  goldPutter, goldWedge, goldDriver, whackOfWarlock,
+  auroritePutter, auroriteWedge, auroriteDriver,  arcaneStrike,
+  mythrilPutter, mythrilWedge, mythrilDriver, LegendaryClub,
+
+
+  
 }
 
 public class ClubFactory
@@ -15,50 +26,126 @@ public class ClubFactory
     switch (clubType)
     {
       //level1
+      case ClubType.stonePutter:
+        return Resources.Load<Club>("Clubs/stonePutter");
+      case ClubType.stoneWedge:
+        return Resources.Load<Club>("Clubs/stoneWedge");
+      case ClubType.stoneDriver:
+        return Resources.Load<Club>("Clubs/stoneDriver");
       case ClubType.Iron7:
         return Resources.Load<Club>("Clubs/7-Iron");
-      case ClubType.baronsBalance:
-        return Resources.Load<Club>("Clubs/baronsBalance");
+
+       //level2
+      case ClubType.ironPutter:
+        return Resources.Load<Club>("Clubs/IronPutter");
+      case ClubType.ironWedge:
+        return Resources.Load<Club>("Clubs/IronWedge");
+      case ClubType.ironDriver:
+        return Resources.Load<Club>("Clubs/IronDriver");
       case ClubType.excaliberSEdge:
         return Resources.Load<Club>("Clubs/excalibersEdge");
-      case ClubType.ironOfStarfall:
-        return Resources.Load<Club>("Clubs/ironOfStarfall");
-      //level2
+     
+
+      //level3
+      case ClubType.bronzePutter:
+        return Resources.Load<Club>("Clubs/bronzePutter");
+      case ClubType.bronzeWedge:
+        return Resources.Load<Club>("Clubs/bronzeWedge");
+       case ClubType.bronzeDriver:
+        return Resources.Load<Club>("Clubs/bronzeDriver");
+      case ClubType.baronsBalance:
+        return Resources.Load<Club>("Clubs/baronsBalance");
+
+      //level4
+
+      case ClubType.silverPutter:
+        return Resources.Load<Club>("Clubs/SilverPutter");
+      case ClubType.silverWedge:
+        return Resources.Load<Club>("Clubs/SilverWedge");
+      case ClubType.silverDriver:
+        return Resources.Load<Club>("Clubs/SilverDriver");
       case ClubType.merlinsWhisper:
         return Resources.Load<Club>("Clubs/merlinsWhisper");
+
+      //level5
+      case ClubType.moonironPutter:
+        return Resources.Load<Club>("Clubs/MoonIronPutter");
+      case ClubType.moonironWedge:
+        return Resources.Load<Club>("Clubs/MoonIronWedge");
+      case ClubType.moonironDriver:
+        return Resources.Load<Club>("Clubs/MoonIronDriver");             
+      case ClubType.ironOfStarfall:
+        return Resources.Load<Club>("Clubs/ironOfStarfall");
+
+      //level6
+       case ClubType.elvensteelPutter:
+        return Resources.Load<Club>("Clubs/ElvenSteelPutter");
+       case ClubType.elvensteelWedge:
+        return Resources.Load<Club>("Clubs/ElvenSteelWedge");
+       case ClubType.elvensteelDriver:
+        return Resources.Load<Club>("Clubs/ElvenSteelDriver");       
       case ClubType.mysticsMallet:
         return Resources.Load<Club>("Clubs/mysticsMallet");
-      case ClubType.whackOfWarlock:
-        return Resources.Load<Club>("Clubs/whackOfWarlock");
+
+      //level7
+      case ClubType.dragonbonePutter:
+        return Resources.Load<Club>("Clubs/DragonbonePutter");
+      case ClubType.dragonboneWedge:
+        return Resources.Load<Club>("Clubs/DragonboneWedge");
+      case ClubType.dragonboneDriver:
+        return Resources.Load<Club>("Clubs/DragonboneDriver");
       case ClubType.royalRumble:
         return Resources.Load<Club>("Clubs/royalRumble");
-      //level3
-      case ClubType.oraclesOddballPutter:
-        return Resources.Load<Club>("Clubs/oraclesOddballPutter");
+
+      //level8
+      case ClubType.goldPutter:
+        return Resources.Load<Club>("Clubs/GoldPutter");
+      case ClubType.goldWedge:
+        return Resources.Load<Club>("Clubs/GoldWedge");
+      case ClubType.goldDriver:
+        return Resources.Load<Club>("Clubs/GoldDriver");
+      case ClubType.whackOfWarlock:
+        return Resources.Load<Club>("Clubs/whackOfWarlock");
+
+      //level9
+      case ClubType.auroritePutter:
+        return Resources.Load<Club>("Clubs/AuroritePutter");
+      case ClubType.auroriteWedge:
+        return Resources.Load<Club>("Clubs/AuroriteWedge");
+      case ClubType.auroriteDriver:
+        return Resources.Load<Club>("Clubs/AuroriteDriver");
       case ClubType.arcaneStrike:
         return Resources.Load<Club>("Clubs/arcaneStrike");
-      case ClubType.wizardsWind:
-        return Resources.Load<Club>("Clubs/wizardsWind");
-      case ClubType.phantomSwing:
-        return Resources.Load<Club>("Clubs/phantomSwing");
-      //level4
+
+      //level10
+      case ClubType.mythrilPutter:
+        return Resources.Load<Club>("Clubs/MythrilPutter");
+      case ClubType.mythrilWedge:
+        return Resources.Load<Club>("Clubs/MythrilWedge");
+      case ClubType.mythrilDriver:
+        return Resources.Load<Club>("Clubs/MythrilDriver");
       case ClubType.LegendaryClub:
         return Resources.Load<Club>("Clubs/LegendaryClub");
-      case ClubType.wedgeOfSarcophagus:
-        return Resources.Load<Club>("Clubs/wedgeOfSarcophagus");
-      case ClubType.anubisDive:
-        return Resources.Load<Club>("Clubs/anubisDive");
-      case ClubType.ironOfNile:
-        return Resources.Load<Club>("Clubs/ironOfNile");
-      //level5
-      case ClubType.ironOfTitan:
-        return Resources.Load<Club>("Clubs/ironOfTitan");
-      case ClubType.zephyrSwing:
-        return Resources.Load<Club>("Clubs/zephyrSwing");
-      case ClubType.aetheriusIron:
-        return Resources.Load<Club>("Clubs/AetheriusIron");
-      case ClubType.boltOfZeus:
-        return Resources.Load<Club>("Clubs/boltOfZeus");
+        
+      
+        
+
+
+
+
+    
+      
+      
+
+     
+    
+     
+    
+  
+     
+     
+    
+     
 
 
       default:
