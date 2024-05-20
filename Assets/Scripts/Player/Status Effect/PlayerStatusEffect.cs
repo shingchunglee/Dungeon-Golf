29c08459c99fc18549074c5e625579dd01b1d5ec
shingchunglee <shingchung.lee@student.uts.edu.au>
2024-05-20 22:17:52 +1000

    add strength effect

--- Assets/Scripts/Player/Status Effect/PlayerStatusEffect.cs
+++ Assets/Scripts/Player/Status Effect/PlayerStatusEffect.cs
@@ -13,6 +13,18 @@ class PlayerStatusEffect
     {
         public StatusEffectType type;
         public int turns;
+
+        public virtual void OnAimEnter() { }
+
+        public virtual void OnAimExit() { }
+
+        public virtual void OnDamageEnemy(EnemyUnit enemy, int damage) { }
+
+        public virtual void OnMoveEnter() { }
+
+        public virtual void OnMoveExit() { }
+
+        public virtual void OnChangeClub(ClubType club) { }
     }
 
     public List<StatusEffect> statusEffects = new();
@@ -22,7 +34,8 @@ class PlayerStatusEffect
         var effectToAdd = statusEffects.Find(x => x.type == effect);
         if (effectToAdd == null)
         {
-            effectToAdd = new StatusEffect() { type = effect, turns = turns };
+            // effectToAdd = new StatusEffect() { type = effect, turns = turns };
+            effectToAdd = Factory(effect, turns);
             statusEffects.Add(effectToAdd);
         }
         else if (effectToAdd.turns < turns)
@@ -43,6 +56,17 @@ class PlayerStatusEffect
         else
         {
             Debug.LogWarning($"PlayerStatusEffectList: Could not remove {effect} because it was not found in the list.");
+        }
+    }
+
+    public StatusEffect Factory(StatusEffectType effect, int turns)
+    {
+        switch (effect)
+        {
+            case StatusEffectType.STRENGTH:
+                return new PlayerStrength(turns);
+            default:
+                return null;
         }
     }
 }
