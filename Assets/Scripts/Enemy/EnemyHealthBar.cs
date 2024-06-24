4a2c882d885b7b42c15cfc5a33ee3636275277a0
Mitch Munro <mitchmunro@gmail.com>
2024-06-25 00:45:24 +1000

    Updated enemy health bar

--- Assets/Scripts/Enemy/EnemyHealthBar.cs
+++ Assets/Scripts/Enemy/EnemyHealthBar.cs
@@ -7,9 +7,30 @@ using UnityEngine.UI;
 public class EnemyHealthBar : MonoBehaviour
 {
     [SerializeField] private Slider HealthSlider;
+    public bool displayHPBarAtFull = false;
+
+    private void Start()
+    {
+        if (!displayHPBarAtFull)
+        {
+            HealthSlider.gameObject.SetActive(false);
+        }
+    }
 
     public void UpdateHealthBar(float currentValue, float maxValue)
     {
-        HealthSlider.value = currentValue / maxValue;
+        float newValue = currentValue / maxValue;
+
+        if (newValue == 1)
+        {
+            HealthSlider.gameObject.SetActive(true);
+            HealthSlider.value = newValue;
+            HealthSlider.gameObject.SetActive(false);
+        }
+        else
+        {
+            HealthSlider.gameObject.SetActive(true);
+            HealthSlider.value = newValue;
+        }
     }
 }
